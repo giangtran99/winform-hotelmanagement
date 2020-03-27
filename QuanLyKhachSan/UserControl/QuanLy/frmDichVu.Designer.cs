fd8c927bb215ec7393431b1743fba36e2fc522e5
thanhtung0310 <thanhtung031099@gmail.com>
2020-03-27 17:28:30 +0700

    Thiết kế frmDichVu và thao tác với CSDL (bản 1.0)

--- QuanLyKhachSan/UserControl/QuanLy/frmDichVu.Designer.cs
+++ QuanLyKhachSan/UserControl/QuanLy/frmDichVu.Designer.cs
@@ -28,18 +28,205 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewService = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbID = new System.Windows.Forms.TextBox();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbPrice = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewService)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridViewService
+            // 
+            this.dataGridViewService.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewService.Location = new System.Drawing.Point(380, 26);
+            this.dataGridViewService.Name = "dataGridViewService";
+            this.dataGridViewService.RowHeadersWidth = 51;
+            this.dataGridViewService.RowTemplate.Height = 24;
+            this.dataGridViewService.Size = new System.Drawing.Size(338, 424);
+            this.dataGridViewService.TabIndex = 0;
+            this.dataGridViewService.Click += new System.EventHandler(this.dataGridViewService_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 20F);
+            this.label1.Location = new System.Drawing.Point(67, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FORM DỊCH VỤ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F);
+            this.label2.Location = new System.Drawing.Point(32, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã DV:";
+            // 
+            // txbID
+            // 
+            this.txbID.Font = new System.Drawing.Font("Cambria", 12F);
+            this.txbID.Location = new System.Drawing.Point(122, 93);
+            this.txbID.Name = "txbID";
+            this.txbID.Size = new System.Drawing.Size(218, 31);
+            this.txbID.TabIndex = 1;
+            // 
+            // txbName
+            // 
+            this.txbName.Font = new System.Drawing.Font("Cambria", 12F);
+            this.txbName.Location = new System.Drawing.Point(122, 140);
+            this.txbName.Multiline = true;
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(218, 50);
+            this.txbName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F);
+            this.label3.Location = new System.Drawing.Point(32, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Tên DV:";
+            // 
+            // txbPrice
+            // 
+            this.txbPrice.Font = new System.Drawing.Font("Cambria", 12F);
+            this.txbPrice.Location = new System.Drawing.Point(122, 200);
+            this.txbPrice.Name = "txbPrice";
+            this.txbPrice.Size = new System.Drawing.Size(218, 31);
+            this.txbPrice.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F);
+            this.label4.Location = new System.Drawing.Point(32, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Giá DV:";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.BackColor = System.Drawing.Color.PaleGreen;
+            this.buttonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonEdit.Font = new System.Drawing.Font("Cambria", 12F);
+            this.buttonEdit.Location = new System.Drawing.Point(210, 282);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(106, 52);
+            this.buttonEdit.TabIndex = 5;
+            this.buttonEdit.Text = "SỬA";
+            this.buttonEdit.UseVisualStyleBackColor = false;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.BackColor = System.Drawing.Color.Tomato;
+            this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDelete.Font = new System.Drawing.Font("Cambria", 12F);
+            this.buttonDelete.Location = new System.Drawing.Point(67, 355);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(105, 52);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "XÓA";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.BackColor = System.Drawing.Color.Plum;
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSearch.Font = new System.Drawing.Font("Cambria", 12F);
+            this.buttonSearch.Location = new System.Drawing.Point(210, 355);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(106, 52);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "TÌM KIẾM";
+            this.buttonSearch.UseVisualStyleBackColor = false;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.buttonAdd.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonAdd.Font = new System.Drawing.Font("Cambria", 12F);
+            this.buttonAdd.Location = new System.Drawing.Point(67, 282);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(105, 52);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "THÊM";
+            this.buttonAdd.UseVisualStyleBackColor = false;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 7.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(139, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "clear content....";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // frmDichVu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.txbPrice);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewService);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frmDichVu";
-            this.Size = new System.Drawing.Size(150, 182);
+            this.Size = new System.Drawing.Size(751, 478);
+            this.Load += new System.EventHandler(this.frmDichVu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewService)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewService;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbID;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbPrice;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label5;
     }
 }
--- QuanLyKhachSan/UserControl/QuanLy/frmDichVu.cs
+++ QuanLyKhachSan/UserControl/QuanLy/frmDichVu.cs
@@ -8,14 +8,121 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QuanLyKhachSan.DAO;
+using System.Data.SqlClient;
 
 namespace QuanLyKhachSan.UserControl.QuanLy
 {
     public partial class frmDichVu : DevExpress.XtraEditors.XtraUserControl
     {
+        DataAccessContext dichvu = new DataAccessContext();
+        DataTable dtService = new DataTable();
+
+        string serviceId, name, price;
+
         public frmDichVu()
         {
             InitializeComponent();
         }
+
+        private void frmDichVu_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vui lòng gõ tiếng việt không dấu. Xin cảm ơn!!!", "Thông báo.");
+            //Lấy danh sách thông tin dịch vụ vào bảng bên cạnh
+            SqlDataAdapter da = new SqlDataAdapter("select top 1000 MaDichVu, TENDV, GiaDichVu from DICHVU", DataAccessContext.connection);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            dataGridViewService.DataSource = dt;
+        }
+
+        private void dataGridViewService_Click(object sender, EventArgs e)
+        {
+            //Lấy danh sách thông tin dịch vụ vào bảng bên cạnh
+            SqlDataAdapter da = new SqlDataAdapter("select top 1000 MaDichVu, TENDV, GiaDichVu from DICHVU", DataAccessContext.connection);
+            DataTable dt = new DataTable();
+
+            da.Fill(dt);
+            dataGridViewService.DataSource = dt;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //thêm thông tin dịch vụ ver 1.0 (chưa có kiểm tra thông tin)
+            SqlCommand add = new SqlCommand("insert DICHVU values ('" + txbName.Text + "','" + txbPrice.Text + "')");
+            if (txbName.Text != "" && txbPrice.Text != "")
+            {
+                MessageBox.Show("Thêm thành công. Vui lòng click vào bảng Dịch Vụ bên cạnh để cập nhật!!! ", "Thông báo.");
+                dichvu.executeQuery(add);
+            }
+            else
+                MessageBox.Show("Thêm thất bại. Vui lòng kiểm tra lại!!!", "Lỗi.");
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            //chỉnh sửa thông tin dịch vụ theo MaDichVu
+            if (txbName.Text != "" && txbPrice.Text != "")
+            {
+                name = txbName.Text;
+                price = txbPrice.Text;
+
+                SqlCommand edit = new SqlCommand("Update DICHVU set TENDV = '" + @name + "', GiaDichVu = '" + price + "' where MaDichVu = '" + txbID.Text + "'");
+                if (dichvu.executeQuery(edit) != 0)
+                    MessageBox.Show("Chỉnh sửa thành công. Vui lòng click vào bảng Dịch vụ bên cạnh để cập nhật!!! ", "Thông báo.");
+                else
+                    MessageBox.Show("Chỉnh sửa thất bại. Vui lòng kiểm tra lại!!!", "Lỗi.");
+            }
+            else
+                MessageBox.Show("Chỉnh sửa thất bại. Vui lòng kiểm tra lại!!!", "Lỗi.");
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            txbID.Clear();
+            txbName.Clear();
+            txbPrice.Clear();
+            dtService.Clear();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            SqlCommand delete = new SqlCommand("delete from DICHVU where MaDichVu = '" + txbID.Text + "'");
+            if (txbID.Text != "" && txbName.Text != "" && txbPrice.Text != "")
+            {
+                MessageBox.Show("Xóa thành công. Vui lòng click vào bảng Dịch vụ bên cạnh để cập nhật!!! ", "Thông báo.");
+                dichvu.executeQuery(delete);
+            }
+            else
+                MessageBox.Show("Xóa thất bại. Vui lòng kiểm tra lại!!!", "Lỗi.");
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            txbName.Clear();
+            txbPrice.Clear();
+
+            //ấn nút tìm kiếm để lấy thông tin dịch vụ theo MaDichVu
+            string search = "select * from DICHVU where MaDichVu = '" + txbID.Text + "'";
+            dichvu.readDatathroughAdapter(search, dtService);
+            if (dtService.Rows.Count != 0 && txbName.Text == "" && txbPrice.Text == "")
+            {
+                MessageBox.Show("Tìm kiếm thành công!!", "Thông báo.");
+
+                //lấy thông tin Dịch vụ từ dtb
+                name = dtService.Rows[0]["TENDV"].ToString();
+                price = dtService.Rows[0]["GiaDichVu"].ToString();
+
+                //hiện thông tin DichVu
+                txbName.Text = name;
+                txbPrice.Text = price;
+            }
+            else
+            {
+                MessageBox.Show("Tìm kiếm thất bại. Vui lòng kiểm tra lại!!", "Lỗi.");
+            }
+        }
+
     }
 }
