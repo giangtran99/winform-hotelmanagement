b917e54dc53b851a01d7d2c93122f9061ccaaaf0
viethoang99 <viet.mta.hp@gmail.com>
2020-03-27 15:18:19 +0700

    Hoàn thành xong chức nưng tìm kiếm, sửa xóa

--- QuanLyKhachSan/UserControl/QuanLy/frmKhachHang.Designer.cs
+++ QuanLyKhachSan/UserControl/QuanLy/frmKhachHang.Designer.cs
@@ -28,18 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
+            this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txbTimKiem = new System.Windows.Forms.TextBox();
+            this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dGVTimKiem = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
+            this.xtraTabControl1.SuspendLayout();
+            this.xtraTabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVTimKiem)).BeginInit();
             this.SuspendLayout();
+            // 
+            // xtraTabControl1
+            // 
+            this.xtraTabControl1.Location = new System.Drawing.Point(3, 3);
+            this.xtraTabControl1.Name = "xtraTabControl1";
+            this.xtraTabControl1.SelectedTabPage = this.xtraTabPage1;
+            this.xtraTabControl1.Size = new System.Drawing.Size(182, 522);
+            this.xtraTabControl1.TabIndex = 0;
+            this.xtraTabControl1.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.xtraTabPage1,
+            this.xtraTabPage2});
+            // 
+            // xtraTabPage1
+            // 
+            this.xtraTabPage1.Controls.Add(this.label1);
+            this.xtraTabPage1.Controls.Add(this.button1);
+            this.xtraTabPage1.Controls.Add(this.txbTimKiem);
+            this.xtraTabPage1.Name = "xtraTabPage1";
+            this.xtraTabPage1.Size = new System.Drawing.Size(176, 494);
+            this.xtraTabPage1.Text = "Tìm Kiếm NV";
+            this.xtraTabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.XtraTabPage1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nhập thông tin tìm kiếm:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tìm Kiếm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // txbTimKiem
+            // 
+            this.txbTimKiem.Location = new System.Drawing.Point(12, 98);
+            this.txbTimKiem.Multiline = true;
+            this.txbTimKiem.Name = "txbTimKiem";
+            this.txbTimKiem.Size = new System.Drawing.Size(154, 31);
+            this.txbTimKiem.TabIndex = 0;
+            // 
+            // xtraTabPage2
+            // 
+            this.xtraTabPage2.Name = "xtraTabPage2";
+            this.xtraTabPage2.Size = new System.Drawing.Size(176, 494);
+            this.xtraTabPage2.Text = "Sửa, Xóa NV";
+            this.xtraTabPage2.Paint += new System.Windows.Forms.PaintEventHandler(this.XtraTabPage2_Paint_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(181, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(662, 516);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // dGVTimKiem
+            // 
+            this.dGVTimKiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGVTimKiem.Location = new System.Drawing.Point(193, 12);
+            this.dGVTimKiem.Name = "dGVTimKiem";
+            this.dGVTimKiem.Size = new System.Drawing.Size(652, 506);
+            this.dGVTimKiem.TabIndex = 1;
+            this.dGVTimKiem.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGVTimKiem_CellContentClick);
+            this.dGVTimKiem.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGVTimKiem_CellContentDoubleClick);
             // 
             // frmKhachHang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dGVTimKiem);
+            this.Controls.Add(this.xtraTabControl1);
             this.Name = "frmKhachHang";
-            this.Size = new System.Drawing.Size(382, 242);
+            this.Size = new System.Drawing.Size(856, 528);
+            ((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
+            this.xtraTabControl1.ResumeLayout(false);
+            this.xtraTabPage1.ResumeLayout(false);
+            this.xtraTabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGVTimKiem)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
+        private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dGVTimKiem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txbTimKiem;
     }
 }
--- QuanLyKhachSan/UserControl/QuanLy/frmKhachHang.cs
+++ QuanLyKhachSan/UserControl/QuanLy/frmKhachHang.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QuanLyKhachSan.DAO;
+using System.Data.SqlClient;
 
 namespace QuanLyKhachSan.UserControl
 {
@@ -16,6 +18,129 @@ namespace QuanLyKhachSan.UserControl
         public frmKhachHang()
         {
             InitializeComponent();
+        }
+
+        private void TextBoxTimKiem_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+        SqlCommandBuilder sqlCommand = null;
+
+        SqlDataAdapter sqlAdapter = null;
+
+        DataSet dataset = null;
+        public void LoadData()
+        {
+            try
+            {
+                sqlAdapter = new SqlDataAdapter("SELECT MaNV 'Mã nhân viên', HoTen 'Họ và tên', NgaySinh 'Ngày sinh', GioiTinh 'Giới tính', SDT 'Số điện thoại' , 'Delete' AS [Thao tác] FROM NHANVIEN", DataAccessContext.connection);
+                sqlCommand = new SqlCommandBuilder(sqlAdapter);
+                sqlAdapter.UpdateCommand = sqlCommand.GetUpdateCommand();
+                sqlAdapter.DeleteCommand = sqlCommand.GetDeleteCommand();
+                dataset = new DataSet();
+                sqlAdapter.Fill(dataset, "NHANVIEN");
+                dGVTimKiem.DataSource = null;
+                dGVTimKiem.DataSource = dataset.Tables["NHANVIEN"];
+                for (int i = 0; i < dGVTimKiem.Rows.Count; i++)
+                {
+                    DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                    dGVTimKiem[5, i] = linkCell;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void dGVTimKiem_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 5)
+                {
+                    string Task = dGVTimKiem.Rows[e.RowIndex].Cells[5].Value.ToString();
+                    if (Task == "Delete")
+                    {
+                        if (MessageBox.Show("Bạn có chắc chắm muốn xóa không?", "Đang xóa...", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            int rowIndex = e.RowIndex;
+                            dGVTimKiem.Rows.RemoveAt(rowIndex);
+                            dataset.Tables["NHANVIEN"].Rows[rowIndex].Delete();
+                            sqlAdapter.Update(dataset, "NHANVIEN");
+                        }
+                    }
+                    else
+                    {
+                        int r = e.RowIndex;
+                        DataAccessContext dt = new DataAccessContext();
+                        SqlCommand cmd = new SqlCommand("Update NHANVIEN SET HoTen = @HoTen, NgaySinh = @NgaySinh, GioiTinh = @GioiTinh, SDT = @SDT WHERE MaNV = @MaNV");
+                        cmd.Parameters.Add("@MaNV", dGVTimKiem.Rows[r].Cells["Mã nhân viên"].Value);
+                        cmd.Parameters.Add("@HoTen", dGVTimKiem.Rows[r].Cells["Họ và tên"].Value);
+                        cmd.Parameters.Add("@NgaySinh", dGVTimKiem.Rows[r].Cells["Ngày sinh"].Value);
+                        cmd.Parameters.Add("@GioiTinh", dGVTimKiem.Rows[r].Cells["Giới tính"].Value);
+                        cmd.Parameters.Add("@SDT", dGVTimKiem.Rows[r].Cells["Số điện thoại"].Value);
+                        dt.executeSelectQuery(cmd);
+                        dGVTimKiem.Rows[e.RowIndex].Cells[5].Value = "Delete";
+                        MessageBox.Show("Đã cập nhật thành công!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void dGVTimKiem_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int lastRow = e.RowIndex;
+                DataGridViewRow nRow = dGVTimKiem.Rows[lastRow];
+                DataGridViewLinkCell linkCell = new DataGridViewLinkCell();
+                dGVTimKiem[5, lastRow] = linkCell;
+                nRow.Cells["Thao tác"].Value = "Update";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void XtraTabPage2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            string search = txbTimKiem.Text.Trim();
+            DataAccessContext oop = new DataAccessContext();
+            DataTable dataTable = new DataTable();
+
+            oop.readDatathroughAdapter("SELECT MaNV 'Mã nhân viên', HoTen 'Họ và tên', NgaySinh 'Ngày sinh', GioiTinh 'Giới tính', SDT 'Số điện thoại' FROM NHANVIEN WHERE HoTen like N'%" + search + "%' or GioiTinh like N'%" + search + "%' or SDT like '%" + search + "%'", dataTable);
+            dGVTimKiem.DataSource = dataTable;
+        }
+
+        private void XtraTabPage2_Paint_1(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                LoadData();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void XtraTabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            DataAccessContext oop = new DataAccessContext();
+            DataTable dataTable = new DataTable();
+
+            oop.readDatathroughAdapter("SELECT MaNV 'Mã nhân viên', HoTen 'Họ và tên', NgaySinh 'Ngày sinh', GioiTinh 'Giới tính', SDT 'Số điện thoại' FROM NHANVIEN", dataTable);
+            dGVTimKiem.DataSource = dataTable;
         }
     }
 }
